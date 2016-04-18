4474b5a0c3c431b1f9d17d8aec52e2058289d796
Perry Anderson <perry.anderson@gmail.com>
2016-04-18 18:53:54 -0230

    Added app.js

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function (socket) {
+	console.log('User connected vis socket.io!');
+
+	socket.on('message', function(message){
+		console.log('Message received: ' + message.text);
+		socket.broadcast.emit('message',message);
+	});
+
+	socket.emit('message',{
+		text: 'Welcome to the chat application!'
+	});
+
+});
 
 http.listen(PORT, function () {
 	console.log('Server started!');
