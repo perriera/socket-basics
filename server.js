633f3cf5f0c87c0cb04e59983f185c8020aaa58b
Perry Anderson <perry.anderson@gmail.com>
2016-04-25 18:31:12 -0230

    Added currentUsers command

--- server.js
+++ server.js
@@ -11,17 +11,44 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+function aMessage(who,what) {
+	return {
+		name: who,
+		text: what,
+		timestamp: moment().valueOf()
+	}
+}
+
+function aSystemMessage(what) {
+	return aMessage('System',what);
+}
+
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+	if ( typeof info === 'undefined' ) {
+		return ;
+	}
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+		if ( info.room === userInfo.room ) {
+			users.push(userInfo.name);
+		}
+	});
+	socket.emit('message',
+		aSystemMessage('@currentUsers: '+users.join(', '))
+	);
+}
+
 io.on('connection', function (socket) {
 	console.log('User connected vis socket.io!');
 
 	socket.on('disconnect', function () {
 		if (typeof clientInfo[socket.id] !== 'undefined' ) {
 			socket.leave(clientInfo[socket.id].room);
-			io.to(clientInfo[socket.id].room).emit('message', {
-				name: 'System',
-				text: clientInfo[socket.id].name + ' has left!',
-				timestamp: moment().valueOf()
-			});
+			io.to(clientInfo[socket.id].room).emit('message', 
+				aSystemMessage(clientInfo[socket.id].name + ' has left!')
+			);
 			delete clientInfo[socket.id];
 		}
 	});
@@ -29,26 +56,23 @@ io.on('connection', function (socket) {
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
-		socket.broadcast.to(req.room).emit('message',{
-			name: 'System',
-			text: req.name + ' has joined!',
-			timestamp: moment().valueOf()
-		});
+		socket.broadcast.to(req.room).emit('message', 
+			aSystemMessage(req.name + ' has joined!')
+		);
 	});
 
 	socket.on('message', function(message){
 		console.log('Message received: ' + message.text);
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message',message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message',message);
+		}
 	});
 
-	var now = moment();
-
-	socket.emit('message',{
-		name: 'System',
-		text: 'Welcome to the chat application!',
-		now: now
-	});
+	socket.emit('message',
+		aSystemMessage('Welcome to the chat application!'));
 
 });
 
